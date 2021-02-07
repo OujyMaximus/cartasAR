dde1ad933043fa1eb98a5b9253fe1550f29c66e9
OujyMaximus <oujy93@gmail.com>
2021-02-07 22:42:29 +0100

    Comenzada la refactorización del c

--- CartasAR/Assets/Scripts/ButtonInteraction.cs
+++ CartasAR/Assets/Scripts/ButtonInteraction.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SpatialTracking;
 using UnityEngine.UI;
 
 public class ButtonInteraction : MonoBehaviour
 {
-    public bool isActive = true;
-    public GameObject placementIndicator;
-    public GameObject cardPrefab;
+
+    /*
+     * 
+     * CAMBIAR TODAS LAS FUNCIONES DE BUTTONINTERACTION AL PLAYERDETECT
+     * 
+     */
+
+    private GameObject placementIndicator;
+    private GameObject cardPrefab;
+    private GameObject arCamera;
+    private GameObject cardPositionGO;
+    private TrackedPoseDriver aRTrackedPoseDriver;
+
+    private bool isActive;
+    private GameObject[] cardsInstatiated;
     private GameObject cardInstantiate;
     private GameObject cardInstantiate2;
     private GameObject cardInstantiate3;
 
-    public GameObject arCamera;
-    public GameObject cardPositionGO;
-    TrackedPoseDriver aRTrackedPoseDriver;
 
     private bool cardSelected;
     private Vector3 playerPosition;
@@ -22,24 +32,35 @@ public class ButtonInteraction : MonoBehaviour
     private Vector3 cardPosition;
     private Quaternion cardRotation;
 
+    private UnityAction<bool, Image> buttonPlacementPress;
+
+    public ButtonInteraction(
+                        GameObject placementIndicator,
+                        GameObject cardPrefab,
+                        GameObject arCamera,
+                        GameObject cardPositionGO)
+    {
+        this.placementIndicator = placementIndicator;
+        this.cardPrefab = cardPrefab;
+        this.arCamera = arCamera;
+        this.cardPositionGO = cardPositionGO;
+    }
+
     void Awake()
     {
+        isActive = true;
         cardSelected = false;
+
         aRTrackedPoseDriver = arCamera.GetComponent<TrackedPoseDriver>();
     }
 
+    //----------------------------------------------
+    //METHODS
+    //----------------------------------------------
+
     public void ButtonPlacementPress()
     {
-        isActive = !isActive;
-        placementIndicator.SetActive(isActive);
-        if (isActive)
-        {
-            GetComponent<Image>().color = new Color(0.2926f, 1f, 0.033f, 1f);
-        }
-        else
-        {
-            GetComponent<Image>().color = new Color(1f, 0.3537f, 0.3537f, 1f);
-        }
+        buttonPlacementPress?.Invoke(isActive, GetComponent<Image>());
     }
 
     //Este metodo se activa al pulsar el boton de mazo y indica si hay que actualizar la posición de las cartas
@@ -47,36 +68,6 @@ public class ButtonInteraction : MonoBehaviour
     {
         cardSelected = !cardSelected;
 
-        if (cardSelected)
-        {
-            cardPosition = cardPositionGO.transform.position;
-            //cardRotation = new Quaternion(1 - playerRotation.x, 1 - playerRotation.y, 1 - playerRotation.z, 1 - playerRotation.w);
-            cardRotation = cardPositionGO.transform.rotation;
-
-            cardInstantiate = GameObject.Instantiate(cardPrefab, cardPosition, cardRotation);
-            cardInstantiate.transform.SetParent(cardPositionGO.transform);
-
-            Vector3 cardPosition2 = new Vector3(cardInstantiate.transform.localPosition.x - 0.08f, cardInstantiate.transform.localPosition.y, cardInstantiate.transform.localPosition.z - 0.05f);
-
-            cardInstantiate2 = GameObject.Instantiate(cardPrefab, cardPosition, cardRotation);
-            cardInstantiate2.transform.SetParent(cardPositionGO.transform);
-            cardInstantiate2.transform.localPosition = cardPosition2;
-
-            Vector3 cardPosition3 = new Vector3(cardInstantiate.transform.localPosition.x - (0.08f * 2), cardInstantiate.transform.localPosition.y, cardInstantiate.transform.localPosition.z - 0.05f);
-
-            cardInstantiate3 = GameObject.Instantiate(cardPrefab, cardPosition, cardRotation);
-            cardInstantiate3.transform.SetParent(cardPositionGO.transform);
-            cardInstantiate3.transform.localPosition = cardPosition3;
-        }
-        else
-        {
-            if (cardInstantiate != null)
-                Destroy(cardInstantiate);
-            if (cardInstantiate2 != null)
-                Destroy(cardInstantiate2);
-            if (cardInstantiate3 != null)
-                Destroy(cardInstantiate3);
-        }
     }
 
     public bool GetCardSelectStatus() => cardSelected;
---
+++ CartasAR/Assets/Scripts/GameFunctions.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+using UnityEngine.UI;
+
+public class GameFunctions : MonoBehaviour
+{
+    #region Scripts variables
+    private PlaceOnPlane placeOnPlane;
+    private PlayerDetect playerDetect;
+    #endregion
+
+    #region Scene GameObject variables
+    public GameObject placementIndicator;
+    public GameObject cardPrefab;
+    public GameObject arCamera;
+    public GameObject cardPositionGO;
+    public TrackedPoseDriver aRTrackedPoseDriver;
+    #endregion
+
+    #region ButtonInteraction variables
+
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //----------------------------------------------
+    //BUTTON INTERACTION
+    //----------------------------------------------
+
+    public void ButtonPlacementPress(bool isActive, Image image)
+    {
+        isActive = !isActive;
+        placementIndicator.SetActive(isActive);
+        if (isActive)
+        {
+            image.color = new Color(0.2926f, 1f, 0.033f, 1f);
+        }
+        else
+        {
+            image.color = new Color(1f, 0.3537f, 0.3537f, 1f);
+        }
+    }
+
+    //----------------------------------------------
+
+    //Este metodo se activa al pulsar el boton de mazo y indica si hay que actualizar la posición de las cartas
+    public GameObject[] ButtonSelectCardPress(bool cardSelected)
+    {
+        GameObject[] cardsInstantiated = new GameObject[3];
+        Vector3 cardPosition;
+        Quaternion cardRotation;
+
+        if (cardSelected)
+        {
+            cardPosition = cardPositionGO.transform.position;
+            cardRotation = cardPositionGO.transform.rotation;
+
+            cardsInstantiated[0] = GameObject.Instantiate(cardPrefab, cardPosition, cardRotation);
+            cardInstantiate.transform.SetParent(cardPositionGO.transform);
+
+            Vector3 cardPosition2 = new Vector3(cardInstantiate.transform.localPosition.x - 0.08f, cardInstantiate.transform.localPosition.y, cardInstantiate.transform.localPosition.z - 0.05f);
+
+            cardInstantiate2 = GameObject.Instantiate(cardPrefab, cardPosition, cardRotation);
+            cardInstantiate2.transform.SetParent(cardPositionGO.transform);
+            cardInstantiate2.transform.localPosition = cardPosition2;
+
+            Vector3 cardPosition3 = new Vector3(cardInstantiate.transform.localPosition.x - (0.08f * 2), cardInstantiate.transform.localPosition.y, cardInstantiate.transform.localPosition.z - 0.05f);
+
+            cardInstantiate3 = GameObject.Instantiate(cardPrefab, cardPosition, cardRotation);
+            cardInstantiate3.transform.SetParent(cardPositionGO.transform);
+            cardInstantiate3.transform.localPosition = cardPosition3;
+        }
+        else
+        {
+            if (cardInstantiate != null)
+                Destroy(cardInstantiate);
+            if (cardInstantiate2 != null)
+                Destroy(cardInstantiate2);
+            if (cardInstantiate3 != null)
+                Destroy(cardInstantiate3);
+        }
+    }
+}
